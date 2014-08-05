405953ccb2712f735b3485bd1178cc5db77920c2
Tobias <martin.tobias@gmx.net>
2014-08-05 13:40:38 +0200

    basic state machine process started

--- src/de/tu_darmstadt/gdi1/pacman/main/Main.java
+++ src/de/tu_darmstadt/gdi1/pacman/main/Main.java
@@ -1,14 +1,28 @@
 package de.tu_darmstadt.gdi1.pacman.main;
 
 import org.newdawn.slick.AppGameContainer;
+import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.SlickException;
+import org.newdawn.slick.state.StateBasedGame;
 
 import de.tu_darmstadt.gdi1.pacman.view.Pacman;
+import eea.engine.entity.StateBasedEntityManager;
 
-public class Main
+public class Main extends StateBasedGame
 {
-	public static void main(String[] args) throws SlickException
-	{
+	
+	public Main(String name) {
+		super("Pacman");
+		// TODO Auto-generated constructor stub
+	}
+
+	// Jeder State wird durch einen Integer-Wert gekennzeichnet
+    public static final int MAINMENU_STATE = 0;
+    public static final int GAMEPLAY_STATE = 1;
+	
+    public static void main(String[] args) throws SlickException
+	{	 
+		
 		// standardpfade initialisieren
 		setPaths();
 
@@ -34,5 +48,19 @@ public class Main
 		else
 			System.setProperty("org.lwjgl.librarypath",
 			System.getProperty("user.dir") + "/native/" + System.getProperty("os.name").toLowerCase());
+	}
+
+	@Override
+	public void initStatesList(GameContainer arg0) throws SlickException {
+
+		// Fuege dem StateBasedGame die States hinzu 
+		// (der zuerst hinzugefuegte State wird als erster State gestartet)
+		addState(new MainMenuStates(MAINMENU_STATE));
+        addState(new GamePlayState(GAMEPLAY_STATE));
+        
+        // Fuege dem StateBasedEntityManager die States hinzu
+        StateBasedEntityManager.getInstance().addState(MAINMENU_STATE);
+        StateBasedEntityManager.getInstance().addState(GAMEPLAY_STATE);
+				
 	}
 }
