2b9e161a6c2855e6426d82c0321bf3cd3c1eb413
Nossair Ouladali <n.ouladali@hotmail.de>
2014-08-07 14:51:59 +0200

    enemy hinzugefügt

--- src/de/tu_darmstadt/gdi1/pacman/view/GameplayState.java
+++ src/de/tu_darmstadt/gdi1/pacman/view/GameplayState.java
@@ -29,11 +29,12 @@ import eea.engine.event.basicevents.KeyPressedEvent;
 public class GameplayState extends BasicGameState {
 
 	public static Input in; // variable für die key events
-	public static int x; // repräsentiert die position auf der x achse
-	public static int y; // repräsentiert die position auf der y achse
-	public static int x1;
-	public static int y1;
+	public static int px; // repräsentiert die position auf der x achse
+	public static int py; // repräsentiert die position auf der y achse
+	public static int ex1; // repräsentiert die position auf der x achse
+	public static int ey1; // repräsentiert die position auf der y achse
 	public Image playerImg; // stellt das pacman ding da
+	public Image enemyImg;
 	public Image lab;
 	protected int stateID; // stellt ein state da
 	protected StateBasedEntityManager entityManager; // nimmt die entitäten auf
@@ -76,13 +77,16 @@ public class GameplayState extends BasicGameState {
 		// nötig für die Keyevents
 		in = container.getInput();
 		// startpunkte des Pacmans
-		x = 10;
-		y = 10;
-		x1 = 0;
-		y1 = 0;
+		px = 10;
+		py = 10;
+
+		ex1 = (int)((Math.random()) * 801 + 0); 
+		ey1 = (int)((Math.random()) * 601 + 0); 
 		// bild des pacmans
 		playerImg = new Image("/res/pictures/menu/Pacman.jpg");
 		lab = new Image("/res/pictures/menu/X0.png");
+		enemyImg = new Image("/res/pictures/theme1/entities/G0.png");
+		
 	}
 
 	/**
@@ -97,27 +101,42 @@ public class GameplayState extends BasicGameState {
 
 		if (in.isKeyDown(Input.KEY_UP) || in.isKeyDown(Input.KEY_W)) {
 			playerImg = new Image("/res/pictures/menu/Pacman2.jpg");
-			y -= 3;
+			py -= 3;
 		}
 		if (in.isKeyDown(Input.KEY_DOWN) || in.isKeyDown(Input.KEY_S)) {
 			playerImg = new Image("/res/pictures/menu/Pacman1.jpg");
-			y += 3;
+			py += 3;
 		}
 		if (in.isKeyDown(Input.KEY_LEFT) || in.isKeyDown(Input.KEY_A)) {
 			playerImg = new Image("/res/pictures/menu/Pacman3.jpg");
-			x -= 3;
+			px -= 3;
 		}
 		if (in.isKeyDown(Input.KEY_RIGHT) || in.isKeyDown(Input.KEY_D)) {
 			playerImg = new Image("/res/pictures/menu/Pacman.jpg");
-			x += 3;
+			px += 3;
 		}
 		
-		// die weltabgrenzung
-		if (x < 0)   x = 0;
-		if (y < 0)   y = 0;
-		if (x >= 770) x = 770;//sehr unsaubere lösung
-		if (y >= 570) y = 570;// sehr unsaubere lösung
 		
+
+
+		// die weltabgrenzung
+		if (px < 0)
+			px = 0;
+		if (py < 0)
+			py = 0;
+		if (px >= 770)
+			px = 770;// sehr unsaubere lösung
+		if (py >= 570)
+			py = 570;// sehr unsaubere lösung
+		
+		if (ex1 < 0)
+			ex1 = 0;
+		if (ey1 < 0)
+			ey1 = 0;
+		if (ex1 >= 770)
+			ex1 = 770;// sehr unsaubere lösung
+		if (ey1 >= 570)
+			ey1 = 570;// sehr unsaubere lösung
 
 	}
 
@@ -129,11 +148,12 @@ public class GameplayState extends BasicGameState {
 			throws SlickException {
 
 		Reader reader = null;
+		int x1 = 100;
+		int y1 = 100;
 		try {
 			reader = new FileReader("res/levels/Minimal.txt");
 			for (int c; (c = reader.read()) != (1 * -1);) {
-				x1 = x1 + 30;
-				y1 = y1 + 30;
+				x1 = x1 + 10;
 				if (c == 'X')
 					g.drawImage(lab, x1, y1);
 			}
@@ -151,7 +171,8 @@ public class GameplayState extends BasicGameState {
 		entityManager.renderEntities(container, game, g);
 
 		// pacman wird an den X und Y koordianten gespawnt und gezeichnet
-		g.drawImage(playerImg, x, y);
+		g.drawImage(playerImg, px, py);
+		g.drawImage(enemyImg, ex1, ey1);
 	}
 
 	@Override
