df8af1a9d4617ffcc42a86f4eb787c6c6bf465c6
Gdislave <kettensege@gmx.de>
2014-08-09 20:45:06 +0200

    Package(minimal) angelegt mit Klasse LibraryFileReader für das Einlesen der Karte. Main editiert um das Array anzulegen

--- src/de/tu_darmstadt/gdi1/pacman/main/Main.java
+++ src/de/tu_darmstadt/gdi1/pacman/main/Main.java
@@ -1,16 +1,16 @@
 package de.tu_darmstadt.gdi1.pacman.main;
 
+import java.util.ArrayList;
+
 import org.newdawn.slick.AppGameContainer;
 import org.newdawn.slick.SlickException;
 
+import de.tu_darmstadt.gdi1.pacman.minimal.LibraryFileReader;
 import de.tu_darmstadt.gdi1.pacman.view.Pacman;
 
-public class Main
-{
-	
-	
-    public static void main(String[] args) throws SlickException
-	{	 
+public class Main {
+
+	public static void main(String[] args) throws SlickException {
 		// standardpfade initialisieren
 		setPaths();
 
@@ -18,26 +18,36 @@ public class Main
 		Pacman game = new Pacman();
 		AppGameContainer app = new AppGameContainer(game);
 
-		// konfiguration festlegen, falls Vollbild benötigt -> app.setDisplayMode(int1,int2,true) ändern
+		// konfiguration festlegen
+		LibraryFileReader reader = new LibraryFileReader("src\\Karte.txt");
+		ArrayList<char[]> karte = reader.readFile();
+
+		// konfiguration festlegen, falls Vollbild benötigt ->
+		// app.setDisplayMode(int1,int2,true) ändern
 		app.setDisplayMode(800, 600, false);
 		app.setShowFPS(false);// FPS anzeigen ;)
 		app.setTargetFrameRate(60);
 		app.start();
 	}
 
-	private static void setPaths()
-	{
+	private static void setPaths() {
 		String osName = System.getProperty("os.name").toLowerCase();
-    	// Setze den library Pfad abhaengig vom Betriebssystem
-    	if (osName.contains("windows")) {
-    		System.setProperty("org.lwjgl.librarypath",System.getProperty("user.dir") + "/lib/lwjgl-2.9.0/native/windows");
-    	} else if (osName.equals("mac os x")) {
-    		System.setProperty("org.lwjgl.librarypath", System.getProperty("user.dir") + "/lib/lwjgl-2.9.0/native/macosx");
-    	} else {
-    		System.setProperty("org.lwjgl.librarypath", System.getProperty("user.dir") + "/lib/lwjgl-2.9.0/native/" +System.getProperty("os.name").toLowerCase() +'"');
-    	}
-		
+		// Setze den library Pfad abhaengig vom Betriebssystem
+		if (osName.contains("windows")) {
+			System.setProperty("org.lwjgl.librarypath",
+					System.getProperty("user.dir")
+							+ "/lib/lwjgl-2.9.0/native/windows");
+		} else if (osName.equals("mac os x")) {
+			System.setProperty("org.lwjgl.librarypath",
+					System.getProperty("user.dir")
+							+ "/lib/lwjgl-2.9.0/native/macosx");
+		} else {
+			System.setProperty("org.lwjgl.librarypath",
+					System.getProperty("user.dir") + "/lib/lwjgl-2.9.0/native/"
+							+ System.getProperty("os.name").toLowerCase() + '"');
+		}
+
 		System.err.println(System.getProperty("org.lwjgl.librarypath"));
-        System.err.println(System.getProperty("java.library.path"));
+		System.err.println(System.getProperty("java.library.path"));
 	}
 }
