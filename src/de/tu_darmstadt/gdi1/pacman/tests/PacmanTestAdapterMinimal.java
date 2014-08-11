ea0f9995875801f70efc5d0e4c536524d0be32e2
Gdislave <kettensege@gmx.de>
2014-08-11 15:35:15 +0200

    Methoden mit Kommentaren versehen

--- src/de/tu_darmstadt/gdi1/pacman/tests/PacmanTestAdapterMinimal.java
+++ src/de/tu_darmstadt/gdi1/pacman/tests/PacmanTestAdapterMinimal.java
@@ -12,8 +12,9 @@ import de.tu_darmstadt.gdi1.pacman.exceptions.ReachabilityException;
 import de.tu_darmstadt.gdi1.pacman.minimal.LibraryFileReader;
 
 public class PacmanTestAdapterMinimal implements PacmanTestInterfaceMinimal {
-	
-	private char[][] unserekarte; 
+
+	// initialisieren einer Kartenvariable für die weitere Verarbeitung
+	private char[][] unserekarte;
 
 	@Override
 	public boolean levelIsValid(String content) {
@@ -30,41 +31,63 @@ public class PacmanTestAdapterMinimal implements PacmanTestInterfaceMinimal {
 
 	}
 
+	/**
+	 * Übergebener String wird an Methode create_map_fromString übergeben um aus
+	 * String ein charArray[][] zu erzeugen. char[][] wird mit Variablennamen
+	 * "karte" gekennzeichnet und über alle Elemente iteriert. Akkumulator zählt
+	 * bei Iteration hoch und gibt Anzahl an P characters zurück.
+	 * 
+	 * @param String
+	 *            content, ist ein String, der eine Karte representiert
+	 * @return Integer result, ist die Anzahl an P character aus dem übergebenen
+	 *         String
+	 */
 	@Override
 	public int levelGetPacmanSpawnCount(String content) {
-		
+
 		int result = 0;
 		char[][] karte = create_map_fromString(content);
-		
-		for(int i = 0; i < karte.length ; i++){
-			for(int j = 0; j< karte[i].length ;j++){
-				
-				if(karte[i][j] == 'P'){
+
+		for (int i = 0; i < karte.length; i++) {
+			for (int j = 0; j < karte[i].length; j++) {
+
+				if (karte[i][j] == 'P') {
 					result++;
 				}
 			}
 		}
-					
-		System.out.println(result);
+
+		System.out.println("PacmanSpawnPunkte: " + result);
 		return result;
 	}
 
+	/**
+	 * Übergebener String wird an Methode create_map_fromString übergeben um aus
+	 * String ein charArray[][] zu erzeugen. char[][] wird mit Variablennamen
+	 * "karte" gekennzeichnet und über alle Elemente iteriert. Akkumulator zählt
+	 * bei Iteration hoch und gibt Anzahl an G characters zurück.
+	 * 
+	 * @param String
+	 *            content, ist ein String, der eine Karte representiert
+	 * @return Integer result, ist die Anzahl an P character aus dem übergebenen
+	 *         String
+	 */
 	@Override
 	public int levelGetGhostSpawnCount(String content) {
-		
+
 		int result = 0;
 		char[][] karte = create_map_fromString(content);
-		
-		for(int i = 0; i < karte.length ; i++){
-			for(int j = 0; j< karte[i].length ;j++){
-				
-				if(karte[i][j] == 'G'){
+
+		for (int i = 0; i < karte.length; i++) {
+			for (int j = 0; j < karte[i].length; j++) {
+
+				if (karte[i][j] == 'G') {
 					result++;
 				}
 			}
 		}
-					
-		System.out.println(result);
+
+		System.out.println("GhostSpawnPunkte: " + result);
 		return result;
 	}
 
@@ -75,7 +98,7 @@ public class PacmanTestAdapterMinimal implements PacmanTestInterfaceMinimal {
 
 	@Override
 	public char getLevelCharAt(int x, int y) {
-		
+
 		System.out.println("getLevelCharAt " + unserekarte[x][y]);
 		return unserekarte[x][y];
 	}
@@ -187,54 +210,66 @@ public class PacmanTestAdapterMinimal implements PacmanTestInterfaceMinimal {
 		// TODO Auto-generated method stub
 
 	}
-	
-	private char[][] create_map_fromString (String eingabekarte){
-		
-		//temporäre Zeile
-		ArrayList<Character> tmpstring = new ArrayList<Character>();
-		char[] tmpstring2;
-		//Arraylist mit einzelnen Zeilen
-		ArrayList<char[]> akkumulator = new ArrayList<char[]>();
-		//Eingabe umgewandelt in ein char array
-		char[] tmpcont = eingabekarte.toCharArray();
-			
-		for(int i = 0 ; i < tmpcont.length ; i++){
 
-			if(tmpcont[i] != '\n')
-				tmpstring.add(tmpcont[i]);
-					
-			if(tmpcont[i] == '\n' || i == tmpcont.length-1){
-				tmpstring2 = new char[tmpstring.size()];
-				
-				for(int j = 0 ; j < tmpstring.size() ; j++){
-					tmpstring2[j] = tmpstring.get(j);
+	private char[][] create_map_fromString(String eingabekarte) {
+
+		// Eingabe eingabekarte umgewandelt in ein char array
+		char[] tmpcont = eingabekarte.toCharArray();
+
+		// temporäre Zeile, ArrayList<Character> ist ein Objekt welches alle
+		// Character einer Zeile(bis \n)
+		// seperat speichert.wird später in akku eingespeist
+
+		ArrayList<Character> tmp_zeile = new ArrayList<Character>();
+
+		char[] tmpcont2;
+
+		// Arraylist mit einzelnen Zeilen
+		ArrayList<char[]> zeilenAkku = new ArrayList<char[]>();
+
+		// iteriert über einzelne Zeichen des Eingabestrings,der in char[]
+		// tmpcont gespeichert ist
+		for (int i = 0; i < tmpcont.length; i++) {
+
+			// fügt so lange kein \n gefunden wird alle chars in einen
+			// Zwischenspeicher, hier tmp_zeile, ein
+			if (tmpcont[i] != '\n')
+				tmp_zeile.add(tmpcont[i]);
+
+			// fügt sobald der Zeilenumbruch ( \n) gefunden wird das bestehende
+			// Array(Zeile) , hier tmp_zeile, in einen anderen dynamischen
+			// Array, hier
+			if (tmpcont[i] == '\n' || i == tmpcont.length - 1) {
+				tmpcont2 = new char[tmp_zeile.size()];
+
+				for (int j = 0; j < tmp_zeile.size(); j++) {
+					tmpcont2[j] = tmp_zeile.get(j);
 				}
 
-				//akkumulator mit einzelnen Zeilen als Elemente drin
-				akkumulator.add(tmpstring2);
-				//tmpstring resetten
-				tmpstring.clear();
+				// akkumulator mit einzelnen Zeilen als Elemente drin
+				zeilenAkku.add(tmpcont2);
+				// tmpstring resetten
+				tmp_zeile.clear();
 			}
 		}
-		
-		
-		char[][] tmpresult = new char[akkumulator.size()][akkumulator.get(0).length];
-		
-		for(int i = 0 ; i < akkumulator.size() ; i++){
-			tmpresult[i] = akkumulator.get(i);
+
+		char[][] tmpresult = new char[zeilenAkku.size()][zeilenAkku.get(0).length];
+
+		for (int i = 0; i < zeilenAkku.size(); i++) {
+			tmpresult[i] = zeilenAkku.get(i);
 		}
-		
-		//Matrix spiegeln an der Achse
+
+		// Matrix spiegeln an der Achse
 		char[][] result = new char[tmpresult[0].length][tmpresult.length];
-		
-		for(int x = 0; x < tmpresult[0].length; x++){
-			for(int y = 0; y < tmpresult.length ;y++){
-				result [x][y] = tmpresult[y][x];			
+
+		for (int x = 0; x < tmpresult[0].length; x++) {
+			for (int y = 0; y < tmpresult.length; y++) {
+				result[x][y] = tmpresult[y][x];
 			}
 		}
-		
+
 		return result;
-		
+
 	}
 
 }
